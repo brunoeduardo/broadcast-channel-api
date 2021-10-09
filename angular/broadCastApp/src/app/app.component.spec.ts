5c65a1fc715751b710d87b19334e283713678186
Bruno Eduardo <bruno@beduardo.com>
2021-10-08 22:29:08 -0300

    ...

--- angular/broadCastApp/src/app/app.component.spec.ts
+++ angular/broadCastApp/src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('broadCastApp');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('broadCastApp app is running!');
-  });
 });
